3cc0728d574e6e8e8c5615839df578a62d66bbe7
jin camou and Sailor Winkelman <sailor@>
2016-08-31 19:42:36 -0700

    Add walrus.html, scripts.jss, bootstrap.css files. Add clickable functions using JQuery

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function() {
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("paragraph");
+  });
+
+  $("img").click(function() {
+    alert("puddle of walruses");
+  });
+});
